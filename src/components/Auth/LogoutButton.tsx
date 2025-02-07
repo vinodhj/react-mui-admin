92805ffd4d55be11bbc623a2bd045fb33907266f
Vinodh <vinodh.jeevanantham@gmail.com>
2025-02-08 04:54:56 +0530

    logout helper done

--- src/components/Auth/LogoutButton.tsx
+++ src/components/Auth/LogoutButton.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { handleLogout } from './logOut';
 
 const LogoutButton: React.FC = () => {
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    localStorage.clear();
-    sessionStorage.clear();
-    // Pass a logout message via navigation state
-    navigate('/', { state: { logoutMessage: 'Logout successfully!' } });
-  };
-
   return (
-    <Button variant="contained" color="primary" onClick={handleLogout}>
+    <Button variant="contained" color="primary" onClick={() => handleLogout(navigate)}>
       Logout
     </Button>
   );
