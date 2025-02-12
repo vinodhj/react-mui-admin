bd56b272d6559564ef12fd9c8783fd67f999a715
Vinodh <vinodh.jeevanantham@gmail.com>
2025-02-12 14:56:26 +0530

    script tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   build: {
+    emptyOutDir: true,
     chunkSizeWarningLimit: 2000,
     outDir: 'dist',
+    // manifest: true,
   },
   plugins: [react()],
 });
