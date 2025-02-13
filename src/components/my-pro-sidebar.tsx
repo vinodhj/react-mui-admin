3745392eaf924cb8317ee04edef7fa84106e1aed
Vinodh <vinodh.jeevanantham@gmail.com>
2025-02-14 02:24:32 +0530

    footer sidbar with sticky and header changed to sticky and menu scrollable tweaks

--- src/components/my-pro-sidebar.tsx
+++ src/components/my-pro-sidebar.tsx
@@ -1,10 +1,12 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { useSidebarContext } from '../hooks/use-sidebar-context';
 import { tokens } from '../theme/main-theme';
+import { useSession } from '../hooks/use-session';
+import { useHandleLogout } from '../utils/log-out';
 
 import React, { FC, useState } from 'react';
 import { Menu, Sidebar, MenuItem, MenuItemStyles } from 'react-pro-sidebar';
-import { useTheme } from '@mui/material';
+import { Avatar, useTheme, Menu as MuiMenu, MenuItem as MuiMenuItem, ListItemIcon } from '@mui/material';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
@@ -14,6 +16,9 @@ import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import SwitchRightOutlinedIcon from '@mui/icons-material/SwitchRightOutlined';
 import SwitchLeftOutlinedIcon from '@mui/icons-material/SwitchLeftOutlined';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import LogoutIcon from '@mui/icons-material/Logout';
+import startCase from 'lodash/startCase';
 
 // Define the props for the sidebar menu item
 interface ItemProps {
@@ -61,13 +66,21 @@ const MyProSidebar: FC = () => {
   const theme = useTheme();
   const mode = theme.palette.mode; // "dark" | "light"
   const colors = tokens(mode);
-  const [selected, setSelected] = useState<string>('Dashboard');
-
-  // Context for RTL and sidebar image settings
+  const { session } = useSession();
   const { sidebarRTL, setSidebarRTL, sidebarImage, collapsed, setCollapsed } = useSidebarContext();
+  const [selected, setSelected] = useState<string>('Dashboard');
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = useHandleLogout();
   const imageUrl = 'https://user-images.githubusercontent.com/25878302/144499035-2911184c-76d3-4611-86e7-bc4e8ff84ff5.jpg';
-
+  const userAvatar = 'https://i.pravatar.cc/300?img=47';
   const border = mode === 'dark' ? '1px solid' + colors.blackWhite[200] : '1px dotted' + colors.blackWhite[200];
 
   return (
@@ -75,9 +88,8 @@ const MyProSidebar: FC = () => {
       sx={{
         position: 'sticky',
         display: 'flex',
-        height: '100vh',
         top: 0,
-        bottom: 0,
+        height: '100vh',
         zIndex: 1200,
       }}
     >
@@ -90,56 +102,241 @@ const MyProSidebar: FC = () => {
         border-right-width={'2px solid' + colors.blackWhite[200]}
         rootStyles={{
           border,
-          // boxShadow: 'none',
         }}
       >
         <Menu menuItemStyles={getMenuItemStyles(mode, colors)}>
-          <MenuItem
-            icon={(() => {
-              if (collapsed) {
-                return <MenuOutlinedIcon onClick={() => setCollapsed(false)} />;
-              } else if (sidebarRTL) {
-                return <SwitchLeftOutlinedIcon onClick={() => setSidebarRTL(!sidebarRTL)} />;
-              } else {
-                return <SwitchRightOutlinedIcon onClick={() => setSidebarRTL(!sidebarRTL)} />;
-              }
-            })()}
-            style={{
-              margin: '10px 0 20px 0',
-              color: colors.grey[100],
+          {/* HEADER: Sticky at top */}
+          <Box
+            sx={{
+              position: 'sticky',
+              top: 0,
+              zIndex: 2,
             }}
           >
-            {!collapsed && (
-              <Box display="flex" justifyContent="space-between" alignItems="center" ml="15px">
-                <Typography
-                  variant="h4"
-                  color={colors.vibrantBlue[500]}
-                  component={RouterLink}
-                  to="/"
-                  sx={{ textDecoration: 'none', fontWeight: 'bold' }}
-                >
-                  Admin
-                </Typography>
-                <IconButton onClick={() => setCollapsed(true)}>
-                  <MenuOutlinedIcon />
-                </IconButton>
-              </Box>
-            )}
-          </MenuItem>
-
-          <Box paddingLeft={collapsed ? undefined : '2%'}>
-            <Item title="Dashboard" to="/dashboard" icon={<HomeOutlinedIcon />} selected={selected} setSelected={setSelected} />
-
-            <Typography variant="h6" color={colors.grey[300]} sx={{ m: '15px 20px 5px 20px' }}>
-              Data
-            </Typography>
-            <Item title="Manage Team" to="/team" icon={<PeopleOutlinedIcon />} selected={selected} setSelected={setSelected} />
-
-            <Typography variant="h6" color={colors.grey[300]} sx={{ m: '15px 20px 5px 20px' }}>
-              Pages
-            </Typography>
-            <Item title="Tracker Expense" to="/form" icon={<PersonOutlinedIcon />} selected={selected} setSelected={setSelected} />
+            <MenuItem
+              icon={(() => {
+                if (collapsed) {
+                  return <MenuOutlinedIcon onClick={() => setCollapsed(false)} />;
+                } else if (sidebarRTL) {
+                  return <SwitchLeftOutlinedIcon onClick={() => setSidebarRTL(!sidebarRTL)} />;
+                } else {
+                  return <SwitchRightOutlinedIcon onClick={() => setSidebarRTL(!sidebarRTL)} />;
+                }
+              })()}
+              style={{
+                margin: '10px 0 20px 0',
+                color: colors.grey[100],
+              }}
+            >
+              {!collapsed && (
+                <Box display="flex" justifyContent="space-between" alignItems="center" ml="15px">
+                  <Typography
+                    variant="h4"
+                    color={colors.vibrantBlue[500]}
+                    component={RouterLink}
+                    to="/"
+                    sx={{ textDecoration: 'none', fontWeight: 'bold' }}
+                  >
+                    Admin
+                  </Typography>
+                  <IconButton onClick={() => setCollapsed(true)}>
+                    <MenuOutlinedIcon />
+                  </IconButton>
+                </Box>
+              )}
+            </MenuItem>
           </Box>
+
+          {/* SCROLLABLE MENU ITEMS */}
+          <Box
+            sx={{
+              overflowY: 'auto',
+              maxHeight: 'calc(100vh - 160px)',
+              '&::-webkit-scrollbar': {
+                width: '4px',
+              },
+              '&::-webkit-scrollbar-track': {
+                backgroundColor: colors.blackWhite[300],
+              },
+              '&::-webkit-scrollbar-thumb': {
+                backgroundColor: colors.greenAccent[400],
+              },
+              '&::-webkit-scrollbar-thumb:hover': {
+                backgroundColor: colors.greenAccent[400],
+              },
+            }}
+          >
+            <Box paddingLeft={collapsed ? undefined : '2%'}>
+              <Item title="Dashboard" to="/dashboard" icon={<HomeOutlinedIcon />} selected={selected} setSelected={setSelected} />
+
+              <Typography variant="h6" color={colors.grey[300]} sx={{ m: '15px 20px 5px 20px' }}>
+                Data
+              </Typography>
+              <Item title="Manage Team" to="/team" icon={<PeopleOutlinedIcon />} selected={selected} setSelected={setSelected} />
+
+              <Typography variant="h6" color={colors.grey[300]} sx={{ m: '15px 20px 5px 20px' }}>
+                Pages
+              </Typography>
+              <Item title="Tracker Expense" to="/form" icon={<PersonOutlinedIcon />} selected={selected} setSelected={setSelected} />
+              {/* Demo: Many items to show scrolling */}
+              {[...Array(15)].map((_, i) => (
+                <Item
+                  key={i}
+                  title={`Menu Item ${i + 1}`}
+                  to={`/menu-item-${i + 1}`}
+                  icon={<PersonOutlinedIcon />}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+              ))}
+            </Box>
+          </Box>
+
+          {/* Footer Section - pinned at the bottom */}
+          {!collapsed ? (
+            <Box
+              sx={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                p: 2,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                gap: 1,
+                // borderTop: '1px solid',
+                borderStyle: 'solid',
+                borderColor: 'divider',
+                backgroundColor: 'transparent',
+              }}
+            >
+              <Box display="flex" alignItems="center" gap={1}>
+                <Avatar src={userAvatar} alt="User Avatar" sx={{ width: 40, height: 40 }} />
+                <Box textAlign="left">
+                  <Typography variant="subtitle1" sx={{ color: colors.vibrantBlue[500], fontWeight: 'bold' }}>
+                    {startCase(session.adminName.toLowerCase())}
+                  </Typography>
+                  <Typography variant="body2" sx={{ color: colors.grey[300] }}>
+                    {session.adminEmail}
+                  </Typography>
+                  {/* <Typography variant="subtitle2" sx={{ color: colors.grey[300] }}>
+                    {startCase(session.adminRole.toLowerCase())}
+                  </Typography> */}
+                </Box>
+              </Box>
+
+              <Box>
+                <IconButton
+                  aria-label="more"
+                  onClick={handleMenuOpen}
+                  sx={{
+                    color: colors.grey[100],
+                    border: '1px solid ' + colors.grey[300],
+                    width: 40,
+                    height: 40,
+                  }}
+                >
+                  <MoreVertIcon />
+                </IconButton>
+                <MuiMenu
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleMenuClose}
+                  anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                  transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                  slotProps={{
+                    paper: {
+                      sx: {
+                        minWidth: 160,
+                        p: 1,
+                      },
+                    },
+                  }}
+                >
+                  <MuiMenuItem onClick={handleMenuClose}>
+                    <ListItemIcon sx={{ color: colors.grey[100] }}>
+                      <PersonOutlinedIcon fontSize="small" />
+                    </ListItemIcon>
+                    <Typography variant="body1" sx={{ color: colors.grey[100] }}>
+                      My Profile
+                    </Typography>
+                  </MuiMenuItem>
+                  <MuiMenuItem
+                    onClick={() => {
+                      handleLogout();
+                      handleMenuClose();
+                    }}
+                  >
+                    <ListItemIcon sx={{ color: colors.grey[100] }}>
+                      <LogoutIcon fontSize="small" />
+                    </ListItemIcon>
+                    <Typography variant="body1" sx={{ color: colors.grey[100] }}>
+                      Logout
+                    </Typography>
+                  </MuiMenuItem>
+                </MuiMenu>
+              </Box>
+            </Box>
+          ) : (
+            // When collapsed: show only the avatar, centered at bottom
+            <Box
+              sx={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                textAlign: 'center',
+                p: 2,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderTop: '1px solid',
+                borderColor: 'divider',
+              }}
+            >
+              <Avatar src={userAvatar} alt="User Avatar" sx={{ width: 40, height: 40, cursor: 'pointer' }} onClick={handleMenuOpen} />
+              <MuiMenu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleMenuClose}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                slotProps={{
+                  paper: {
+                    sx: {
+                      minWidth: 160,
+                      p: 1,
+                    },
+                  },
+                }}
+              >
+                <MuiMenuItem onClick={handleMenuClose}>
+                  <ListItemIcon sx={{ color: colors.grey[100] }}>
+                    <PersonOutlinedIcon fontSize="small" />
+                  </ListItemIcon>
+                  <Typography variant="body1" sx={{ color: colors.grey[100] }}>
+                    My Profile
+                  </Typography>
+
+                  {/* My Profile &nbsp; <PersonOutlinedIcon /> */}
+                </MuiMenuItem>
+                <MuiMenuItem
+                  onClick={() => {
+                    handleLogout();
+                    handleMenuClose();
+                  }}
+                >
+                  <ListItemIcon sx={{ color: colors.grey[100] }}>
+                    <LogoutIcon fontSize="small" />
+                  </ListItemIcon>
+                  <Typography variant="body1" sx={{ color: colors.grey[100] }}>
+                    Logout &nbsp; <LogoutIcon />
+                  </Typography>
+                </MuiMenuItem>
+              </MuiMenu>
+            </Box>
+          )}
         </Menu>
       </Sidebar>
     </Box>
