9db437ad9c00a2462baec85dcd69539d0c1aca49
Vinodh <vinodh.jeevanantham@gmail.com>
2025-02-17 13:59:08 +0530

    jest test added and config for react

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,10 @@ export const presets = [
   [
     '@babel/preset-env',
     {
-      // This tells Babel to keep ES module syntax
-      modules: false,
+      // Transpile modules to CommonJS for Jest compatibility.
+      modules: 'commonjs',
     },
   ],
   '@babel/preset-react',
+  '@babel/preset-typescript', // Add this if you use TypeScript
 ];
