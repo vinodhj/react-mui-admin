cf34600eda5a6fa0a87d2357ebce5e29d11cf6c5
Vinodh <vinodh.jeevanantham@gmail.com>
2025-02-28 03:03:37 +0530

    route team tweaks

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -21,16 +21,18 @@ const AppRoutes: React.FC = () => {
         <Route path="/" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
 
-        {/* Team routes */}
-        <Route path="/team" element={<ProtectedRoute element={<Team />} allowedRoles={['ADMIN']} />} />
-        <Route path="/team/create" element={<ProtectedRoute element={<CreateTeam />} allowedRoles={['ADMIN']} />} />
-        <Route path="/team/:id" element={<ProtectedRoute element={<TeamDetails />} allowedRoles={['ADMIN']} />} />
-        <Route path="/team/edit/:id" element={<ProtectedRoute element={<EditTeam />} allowedRoles={['ADMIN']} />} />
-
         {/* Settings */}
         <Route path="/profile" element={<Profile />} />
         <Route path="/edit-profile" element={<EditProfile />} />
         <Route path="/change-password" element={<ChangePassword />} />
+
+        {/* Team routes */}
+        <Route path="/team">
+          <Route index element={<ProtectedRoute element={<Team />} allowedRoles={['ADMIN']} />} />
+          <Route path="create" element={<ProtectedRoute element={<CreateTeam />} allowedRoles={['ADMIN']} />} />
+          <Route path=":id" element={<ProtectedRoute element={<TeamDetails />} allowedRoles={['ADMIN']} />} />
+          <Route path="edit/:id" element={<ProtectedRoute element={<EditTeam />} allowedRoles={['ADMIN']} />} />
+        </Route>
 
         {/* FAQ */}
         <Route path="/faq" element={<ProtectedRoute element={<Faq />} />} />
