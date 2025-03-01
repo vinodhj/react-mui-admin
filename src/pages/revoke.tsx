7e52fd2c44502e337f0995bfbf93e7fa424801d2
Vinodh <vinodh.jeevanantham@gmail.com>
2025-03-01 23:03:32 +0530

    error app msg fix

--- src/pages/revoke.tsx
+++ src/pages/revoke.tsx
@@ -6,7 +6,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import EngineeringIcon from '@mui/icons-material/Engineering';
 import { useMode } from '../contexts/color-mode-context';
 
-export default function RevokeError({ isRevoked }: Readonly<{ isRevoked: boolean }>) {
+export default function RevokeError({ message }: Readonly<{ message: string }>) {
   const [theme] = useMode();
 
   const handleSignIn = () => {
@@ -34,23 +34,23 @@ export default function RevokeError({ isRevoked }: Readonly<{ isRevoked: boolean
             alignItems: 'center',
           }}
         >
-          {isRevoked ? (
+          {message === 'REVOKE_ERROR' ? (
             <LockOutlinedIcon sx={{ fontSize: 60, color: theme.palette.primary.main, mb: 2 }} />
           ) : (
             <EngineeringIcon sx={{ fontSize: 60, color: theme.palette.primary.main, mb: 2 }} />
           )}
 
           <Typography variant="h4" component="h1" gutterBottom>
-            {isRevoked ? 'Session Revoked' : 'Under Maintenance'}
+            {message === 'REVOKE_ERROR' ? 'Session Expired' : 'Under Maintenance'}
           </Typography>
           <Typography variant="body1" align="center" sx={{ mt: 1 }}>
-            {isRevoked
+            {message === 'REVOKE_ERROR'
               ? 'For security reasons, your session is no longer valid.'
               : 'Due to the required infrastructure upgrades, the application is currently under maintenance.'}
             <br />
-            {isRevoked && 'Please sign in again to continue.'}
+            {message === 'REVOKE_ERROR' && 'Please sign in again to continue.'}
           </Typography>
-          {isRevoked && (
+          {message === 'REVOKE_ERROR' && (
             <Button variant="contained" color="primary" onClick={handleSignIn} sx={{ mt: 4 }}>
               Sign In Again
             </Button>
